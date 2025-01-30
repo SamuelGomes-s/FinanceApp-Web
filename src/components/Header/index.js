dff0f6b71c34671ea79946338c363436e3edd066
samuelg <sam2018gs@gmail.com>
2025-01-29 22:01:19 -0300

    Criando btn de logout

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,18 @@ import { Link } from "react-router-dom";
 import { IoHomeOutline } from "react-icons/io5";
 import { CgProfile } from "react-icons/cg";
 import { TbCashRegister } from "react-icons/tb";
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+
 export default function Header() {
+
+    const { LogOut } = useContext(AuthContext)
+
+    async function handleLogOut(e) {
+        e.preventDefault()
+        await LogOut()
+    }
+
     return (
         <Container>
             <LinkStyled to={'/home'}>
@@ -24,6 +35,9 @@ export default function Header() {
                 </Svg>
                 Perfil
             </LinkStyled>
+            <LogOutBtn onClick={(e) => handleLogOut(e)}>
+                Sair
+            </LogOutBtn>
         </Container>
     )
 }
@@ -66,5 +80,21 @@ const LinkStyled = styled(Link)`
 const Svg = styled.div`
     @media screen and (max-width: 700px) {
         display: none;
+    }
+`;
+
+const LogOutBtn = styled.button`
+    background-color:rgb(168, 27, 27);
+    cursor: pointer;
+    text-decoration: none;
+    color: #E0E5F8 ;
+    transition: all 0.5s;
+    padding: 0.5em;
+    &:hover{
+        text-decoration: underline;
+        background-color:rgba(43, 45, 66, 0.55);
+    }
+    @media screen and (max-width: 700px) {
+        width: 80px;
     }
 `;
